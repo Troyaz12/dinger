c131a2890275131b3c334e2e69be3c1ef770f1d5
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-10-26 12:35:15 +0200

    Migrate to plugin 3.0.0; added Room @Transaction where required

--- data/src/main/kotlin/data/tinder/recommendation/RecommendationUserCommonConnectionDao.kt
+++ data/src/main/kotlin/data/tinder/recommendation/RecommendationUserCommonConnectionDao.kt
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
+import android.arch.persistence.room.Transaction
 
 @Dao
 internal interface RecommendationUserCommonConnectionDao {
@@ -11,6 +12,7 @@ internal interface RecommendationUserCommonConnectionDao {
     fun insertCommonConnection(commonConnection: RecommendationUserCommonConnectionEntity)
 
     @Query("SELECT * from RecommendationUserCommonConnectionEntity WHERE id=:id")
+    @Transaction
     fun selectCommonConnectionById(id: String)
             : List<RecommendationUserCommonConnectionWithRelatives>
 }
